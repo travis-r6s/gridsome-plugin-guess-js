9be23c32fd66bfb93940cc9af5647e1aefa590ce
Travis Reynolds <travis@cirruscreative.co.uk>
2020-01-28 17:45:11 +0000

    Preload component using vue router

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,7 +1,7 @@
 import { guess } from 'guess-webpack/api'
 
 export default function (Vue, options, { isClient, router }) {
-  if (isClient && process.env.NODE_ENV !== 'development') {
+  if (isClient) {
     router.afterEach(() => {
       Vue.nextTick(() => {
         const predictions = Object.keys(guess()).sort((a, b) => a.probability - b.probability)
@@ -9,6 +9,16 @@ export default function (Vue, options, { isClient, router }) {
         for (const path of predictions) {
           console.log(`Prefetching ${path}`)
           Vue.prototype.$fetch(path, { shouldPrefetch: true })
+          router.getMatchedComponents(path).forEach(Component => {
+            if (typeof Component === 'function') {
+              try {
+                Component()
+                Component.__prefetched = true
+              } catch (e) {
+                console.error(`Could not preload ${path}`)
+              }
+            }
+          })
         }
       })
     })
